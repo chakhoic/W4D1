1f9ad24a56005922fc562c19efc9e12aa23bc1a7
chakhoic <chakhoichan@gmail.com>
2022-11-28 14:30:13 -0500

    chak drive lynsie nav

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,12 @@
 class PolyTreeNode
+    def initialize(value)
+        Node.new = hash
+        self.parent = nil
+        self.children = []
+    end
+
+    def parent=
+        node2.parent = node1
+    end
 
 end
